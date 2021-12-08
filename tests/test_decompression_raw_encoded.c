7e1ba82ac07fc4ead49be86b314b5e18f543c8d4
Bryan Pearson <peabryan95@gmail.com>
2021-12-08 18:01:51 -0500

    print out struct contents clearly

--- tests/test_decompression_raw_encoded.c
+++ tests/test_decompression_raw_encoded.c
@@ -38,7 +38,26 @@ int main()
     struct uploadData* decompressed_struct = (struct uploadData*)malloc(struct_len);
     string_to_uploadData(decompressed_struct, decompressed);
 
-    printf("Serial: %s\n", decompressed_struct->serialNum);
+    printf("t1: %f\n", decompressed_struct->avg_temp);
+    printf("p1: %f\n", decompressed_struct->avg_press);
+    printf("h1: %f\n", decompressed_struct->avg_hum);
+    printf("c1: %f\n", decompressed_struct->scd30_ppm);
+    printf("P251: %d\n", decompressed_struct->pm2_5[0]);
+    printf("P252: %d\n", decompressed_struct->pm2_5[1]);
+    printf("P253: %d\n", decompressed_struct->pm2_5[2]);
+    printf("P101: %d\n", decompressed_struct->pm10[0]);
+    printf("P102: %d\n", decompressed_struct->pm10[1]);
+    printf("P103: %d\n", decompressed_struct->pm10[2]);
+    printf("pv: %f\n", decompressed_struct->pv);
+    printf("pc: %f\n", decompressed_struct->pc);
+    printf("pp: %f\n", decompressed_struct->pp);
+    printf("bv: %d\n", decompressed_struct->bv);
+    printf("bc: %d\n", decompressed_struct->bv);
+    printf("pt: %d\n", decompressed_struct->bt);
+    printf("L: %s\n", decompressed_struct->gps);
+    printf("ts: %s\n", decompressed_struct->ts);
+    printf("TZ: %s\n", decompressed_struct->tz);
+    printf("uid: %s\n", decompressed_struct->serialNum);
 
     return 0;
 }
